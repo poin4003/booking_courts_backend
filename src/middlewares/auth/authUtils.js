fdd50808a51ee06185394d0c88876f8278f55959
LVNAnh <levunhatanh1997@gmail.com>
2025-04-25 22:27:33 +0700

    cap nhat them role sau khi login

--- src/middlewares/auth/authUtils.js
+++ src/middlewares/auth/authUtils.js
@@ -1,57 +1,58 @@
-'use strict'
+"use strict";
 
-const JWT = require('jsonwebtoken')
-const { asyncHandler } = require('../../helpers/asyncHandler')
-const { AuthFailureError } = require('../../core/error.response')
+const JWT = require("jsonwebtoken");
+const { asyncHandler } = require("../../helpers/asyncHandler");
+const { AuthFailureError } = require("../../core/error.response");
 
-const createToken = ( payload, jwt_scret_key ) => {
+const createToken = (payload, jwt_scret_key) => {
   try {
-    // Access Token 
+    // Access Token
     const accessToken = JWT.sign(payload, jwt_scret_key, {
-      expiresIn: '30 days'
-    })
+      expiresIn: "30 days",
+    });
 
     JWT.verify(accessToken, jwt_scret_key, (err, decode) => {
       if (err) {
-        console.log('error verify::', err)
+        console.log("error verify::", err);
       } else {
-        console.log('decode verify::', decode)
+        console.log("decode verify::", decode);
       }
-    })
+    });
 
-    return accessToken
+    return accessToken;
   } catch (error) {
-
+    throw new AuthFailureError("Failed to create access token");
   }
-}
+};
 
 const authentication = asyncHandler(async (req, res, next) => {
   // 1. Check userId missing
-  const userId = req.headers[HEADER.CLIENT_ID]
-  if (!userId) throw new AuthFailureError('Invalid requets')
+  const userId = req.headers[HEADER.CLIENT_ID];
+  if (!userId) throw new AuthFailureError("Invalid requets");
 
   // 3. Get and verify token
-  const accessToken = req.headers[HEADER.AUTHORIZATION]
-  if (!accessToken) throw new AuthFailureError('Invalid request')
+  const accessToken = req.headers[HEADER.AUTHORIZATION];
+  if (!accessToken) throw new AuthFailureError("Invalid request");
 
   try {
-    const decodeUser = JWT.verify( accessToken, keyStore.publicKey )
+    const decodeUser = JWT.verify(accessToken, keyStore.publicKey);
     // 4. Check keyStore with userId
-    if (userId !== decodeUser.userId) throw new AuthFailureError('Invalid UserId')
-    req.keyStore = keyStore
-    req.user = decodeUser
-    return next()
+    if (userId !== decodeUser.userId)
+      throw new AuthFailureError("Invalid UserId");
+    req.keyStore = keyStore;
+    req.user = decodeUser;
+    return next();
   } catch (error) {
-    throw error
+    throw error;
   }
-})
+});
 
 const verifyJWT = (token, keySecret) => {
-  return JWT.verify(token, keySecret)
-}
+  return JWT.verify(token, keySecret);
+};
 
 module.exports = {
   createToken,
   authentication,
-  verifyJWT
-}
+  verifyJWT,
+};
