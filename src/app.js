9c73351a72837b2cf9ae21029627d2146fb067b2
Pc_Huy <pchuy4003@gmai.com>
2025-05-10 11:43:56 +0700

    fix cross origin

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({
 
 // Cross origin
 app.use(cors({
-  origin: 'http://localhost:5173, http://bookingcourt.duckdns.org:5173',
+  origin: 'http://localhost:5173, http://bookingcourt.duckdns.org:4173',
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
